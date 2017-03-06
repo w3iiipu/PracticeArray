33be2a47871edbeb27f40d170f4c26369e16d113
w3iiipu <w3iiipu@gmail.com>
2017-03-06 09:09:03 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+        System.out.println(symmetric());
+
+
+    }
+
+
+
+    public static boolean symmetric () {
+        int[][] ar = {
+
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {1, 3, 2, 3},
+                {4, 2, 3, 1}
+        };
+
+        for (int i = 0; i < ar.length; i++){
+            if (ar[i][i] != ar[ar.length-1-i][ar.length-1-i]){
+              return false;
+            }
+        }
+     return true;
+    }
+
+
+}
